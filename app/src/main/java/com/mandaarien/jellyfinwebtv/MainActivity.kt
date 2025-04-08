1db321660098272e4327b703cdb1cbaf93ff657d
mandaarien <obst1603@gmail.com>
2025-04-08 10:05:30 +0200

    change up for android studio

---
+++ app/src/main/java/com/mandaarien/jellyfinwebtv/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.mandaarien.jellyfintv
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        // WebView Settings
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.mediaPlaybackRequiresUserGesture = false
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+
+        // Enable hardware acceleration
+        webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null)
+
+        // WebView Clients
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // Optional: do something after load
+            }
+        }
+
+        // Load local setup screen (initial config)
+        webView.loadUrl("file:///android_asset/www/setup.html")
+    }
+
+    override fun onBackPressed() {
+        // Optional: navigate back in WebView if possible
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
